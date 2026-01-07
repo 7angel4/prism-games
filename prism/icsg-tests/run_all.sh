fcd14531b560bb28985e0ae614b5b49eb4ad9e41
Angel He <angel.heyongxi.2004@gmail.com>
2026-01-07 21:19:51 +0000

    completed scripts for the zero-sum robots case study

--- prism/icsg-tests/run_all.sh
+++ prism/icsg-tests/run_all.sh
@@ -1,3 +1,5 @@
-./icsg-tests/icsgs/robot2.sh
-./icsg-tests/csgs/robot2.sh
+# ./icsg-tests/icsgs/robot2.sh
+# ./icsg-tests/csgs/robot2.sh
 
+./icsg-tests/zero-sum/robot1.sh
+./icsg-tests/zero-sum/robot2.sh
