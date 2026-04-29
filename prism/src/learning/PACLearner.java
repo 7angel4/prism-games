fb1c9e957d5aefd643af3e4da68c4d8bd2eeaac5
Angel He <angel.heyongxi.2004@gmail.com>
2026-04-29 10:02:08 +0100

    sync - reverted to initial CSG solve to initialise MCs

--- prism/src/learning/PACLearner.java
+++ prism/src/learning/PACLearner.java
@@ -337,8 +337,8 @@ public class PACLearner {
         robustMC.setGenStrat(prism.getGenStrat());
         robustMC.setSilentPrecomputations(true);
         robustMC.setVerbosity(0);
-        robustMC.checkExpression(empiricalGame, property.getExpression(), null, true);
-//        robustSolveL1CSG(property); // solve once so that the model checker records the target states
+//        robustMC.checkExpression(empiricalGame, property.getExpression(), null, true);
+        robustSolveL1CSG(property); // solve once so that the model checker records the target states
 
         StateModelChecker csgMC = explicit.StateModelChecker.createModelChecker(empiricalGame.getCentreCSG().getModelType(), prism);
         if (!(csgMC instanceof CSGModelChecker)) {
@@ -350,8 +350,8 @@ public class PACLearner {
         pointMC.setGenStrat(prism.getGenStrat());
         pointMC.setSilentPrecomputations(true);
         pointMC.setVerbosity(0);
-        pointMC.checkExpression(empiricalGame.getCentreCSG(), property.getExpression(), null, true);
-//        solvePointModel(property); // solve once so that the model checker records the target states
+//        pointMC.checkExpression(empiricalGame.getCentreCSG(), property.getExpression(), null, true);
+        solvePointModel(property); // solve once so that the model checker records the target states
 
         helper.targets = robustMC.getTargets();
         if (zeroSum) {
@@ -516,11 +516,11 @@ public class PACLearner {
         prism.initialise();
         prism.useNative();
 
-        Experiment ex = new Experiment(Experiment.CaseStudy.ALOHA);
+        Experiment ex = new Experiment(Experiment.CaseStudy.TRAFFIC_MERGE);
         ex.setSolverString("Yices");
         ex.propertyIndex = 1;
-        ex.robustnessExperiment = true;
-        ex.maxNumEpisodes = 500;
+        ex.robustnessExperiment = false;
+//        ex.maxNumEpisodes = 500;
         Experiment.PacRunSpec spec = ex.buildPacRunSpec(prism);
 
         PACLearner learner = new PACLearner(prism, 41, true);
